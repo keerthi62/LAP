4ef553cf81a2640bd9d037d3ed9901c7db53b580
Keerthi B18049 <68141360+keerthi62@users.noreply.github.com>
2020-12-12 18:04:07 +0530

    Update upload.php

--- admin/upload.php
+++ admin/upload.php
@@ -1,6 +1,6 @@
 <?php
 // $target_dir = "../uploads/";
-$conn = new mysqli("localhost:3306","root","Keerthi@2001","student");
+$conn = new mysqli("g8r9w9tmspbwmsyo.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306","bupdy87dvohbr4iw","gtkjmuz9kqpdnzxg","ovlz54dtty1jll9i");
 extract($_POST);
 $target_file = basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
