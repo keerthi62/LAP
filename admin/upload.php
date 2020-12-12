e392075f61beb65e59171a53359c2384e2c35391
Keerthi B18049 <68141360+keerthi62@users.noreply.github.com>
2020-12-12 19:27:36 +0530

    Update upload.php

--- admin/upload.php
+++ admin/upload.php
@@ -27,7 +27,7 @@ if ($uploadOk == 0) {
   }
   else
   {
-    $query="INSERT INTO files VALUES (urldecode($fileToUpload))";
+    $query="INSERT INTO files VALUES (urlencode($fileToUpload))";
     if ($conn->query($query) === TRUE) {
       echo "The file ". htmlspecialchars($target_file). " has been uploaded.";
       echo " ";
@@ -37,6 +37,6 @@ if ($uploadOk == 0) {
       echo " ";
     }
   }
-  echo urldecode($fileToUpload);
+  echo urlencode($fileToUpload);
 }
 ?>
