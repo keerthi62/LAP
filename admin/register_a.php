d46219c44bc1895216e9d3e7dd3da10bda8b987e
Keerthi B18049 <68141360+keerthi62@users.noreply.github.com>
2020-12-12 13:58:12 +0530

    Update register_a.php

--- admin/register_a.php
+++ admin/register_a.php
@@ -4,7 +4,6 @@ extract($_POST); //imports post variables
 if(isset($save)) //it checks whether we clicked the submit button or not.
 {
 $sql = "SELECT * FROM admindetails"; //selecting all rows from studentdetails.
-$count = $conn->query($sql)->num_rows; //counts number of rows in student details 
 $sql1 = "SELECT * FROM studentdetails where email = '$e'"; //selecting the row from student details where email = entered email.
 $result1 = $conn->query($sql1);
 $sql2 = "SELECT * FROM admindetails where email = '$e'"; //selecting the row from admin details where email = entered email.
@@ -19,7 +18,7 @@ else if($result1->num_rows > 0 or $result3->num_rows > 0) //if user is not a stu
 $msg = "<font color='red'>You cannot register here</font>";
 }
 else{
-$query="INSERT INTO admindetails VALUES ($count+1,'$n','$e','$p',now())"; //inserting the details in specified order.
+$query="INSERT INTO admindetails VALUES ('$n','$e','$p',now())"; //inserting the details in specified order.
 
 
 if ($conn->query($query) === TRUE) { //if details are inserted succesfully 
