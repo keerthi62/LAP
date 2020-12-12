23cdf9eaf80cc0a723f857df481057e7421f72a4
Keerthi B18049 <68141360+keerthi62@users.noreply.github.com>
2020-12-12 19:18:14 +0530

    Update upload.php

--- admin/upload.php
+++ admin/upload.php
@@ -5,28 +5,36 @@ $target_file = basename($fileToUpload);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
-// // Allow certain file formats
-// if($imageFileType == "jpg" && $imageFileType == "png" && $imageFileType == "jpeg"
-// && $imageFileType == "gif" ) {
-//   echo "Sorry, JPG, JPEG, PNG & GIF files are not allowed.";
-//   echo " ";
-//   $uploadOk = 0;
-// }
+// Allow certain file formats
+if($imageFileType == "jpg" && $imageFileType == "png" && $imageFileType == "jpeg"
+&& $imageFileType == "gif" ) {
+  echo "Sorry, JPG, JPEG, PNG & GIF files are not allowed.";
+  echo " ";
+  $uploadOk = 0;
+}
 
-// // Check if $uploadOk is set to 0 by an error
-// if ($uploadOk == 0) {
-//   echo "Sorry, your file was not uploaded.";
-//   echo " ";
-// // if everything is ok, try to upload file
-// } else {
-//   $query="INSERT INTO files VALUES ($fileToUpload)";
-//   if ($conn->query($query) === TRUE) {
-//     echo "The file ". htmlspecialchars($target_file). " has been uploaded.";
-//     echo " ";
-//   } else {
-//     echo "Sorry, there was an error uploading your file.";
-//     echo " ";
-//   }
-// }
-echo $imageFileType;
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+  echo "Sorry, your file was not uploaded.";
+  echo " ";
+// if everything is ok, try to upload file
+} else {
+  $sql = "SELECT * FROM files where filepath = '$fileToUpload'";
+  $result = $conn->query($sql);
+  if ($result->num_rows > 0)
+  {
+    echo "File name already exists";
+  }
+  else
+  {
+    $query="INSERT INTO files VALUES ($fileToUpload)";
+    if ($conn->query($query) === TRUE) {
+      echo "The file ". htmlspecialchars($target_file). " has been uploaded.";
+      echo " ";
+    } else {
+      echo "Sorry, there was an error uploading your file.";
+      echo " ";
+    }
+  }
+}
 ?>
