d93a6aa0a92b9fd2cce29dcc64f7c8097aa34797
Keerthi B18049 <68141360+keerthi62@users.noreply.github.com>
2020-12-12 19:00:35 +0530

    Update sample_a.php

--- admin/sample_a.php
+++ admin/sample_a.php
@@ -8,7 +8,7 @@
 
 <form action="upload.php" method="post" enctype="multipart/form-data"> <!--on form submission it redirects to upload.php-->
   Select file to upload:
-  <input type="file" name="fileToUpload" id="fileToUpload"> <!--upload pdf file or text file -->
+  <input type="file" name="fileToUpload" id="fileToUpload" required> <!--upload pdf file or text file -->
   <input type="submit" value="Upload FIle" name="submit"> <!-- submit file -->
 </form>
 
