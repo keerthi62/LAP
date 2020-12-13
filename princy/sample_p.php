ba2d572b737d7838019a9b3a772f4b65037c2705
Keerthi B18049 <68141360+keerthi62@users.noreply.github.com>
2020-12-13 11:55:00 +0530

    Update sample_p.php

--- princy/sample_p.php
+++ princy/sample_p.php
@@ -3,7 +3,7 @@ $conn = new mysqli("g8r9w9tmspbwmsyo.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:33
 $sql = "SELECT * FROM files";
 if ($result = $conn -> query($sql)) {
   while ($row = $result -> fetch_row()) {
-    echo "<a href='$row[0]' type="application/octet-stream" download>" . basename($row[0]) . "</a><br>";
+    echo "<a href='$row[0]' download>" . basename($row[0]) . "</a><br>";
   }
 }
 ?>
